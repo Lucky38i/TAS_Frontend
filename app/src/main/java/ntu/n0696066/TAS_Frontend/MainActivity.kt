7d7cd59bdfd97224b8c1eb8b0cd9b09a7e9583d9
Alex McBean <al.mcbean@outlook.com>
2020-03-20 18:28:53 +0000

    Migrated to Kotlin

---
+++ app/src/main/java/ntu/n0696066/TAS_Frontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package ntu.n0696066.TAS_Frontend
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
